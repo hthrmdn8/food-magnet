474d1d686f3e3603f5b5951c46ebccdb58341a8a
Rikusen <arigatoriku@aol.com>
2023-07-03 17:35:51 -0500

    Fresh Repository

---
+++ food-magnet/src/main/java/com/github/foodmagnet/FoodMagnetApplication.java
@@ -0,0 +1,13 @@
+package com.github.foodmagnet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodMagnetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodMagnetApplication.class, args);
+	}
+
+}
