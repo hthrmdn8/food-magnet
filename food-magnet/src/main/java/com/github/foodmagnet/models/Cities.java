019a79f470549a6efa0d9341b34d4fe2074aaa1a
“juniperjer” <115501141+juniperjer@users.noreply.github.com>
2023-07-10 17:54:55 -0500

    Created Cities.java with cities

---
+++ food-magnet/src/main/java/com/github/foodmagnet/models/Cities.java
@@ -0,0 +1,47 @@
+public class Cities{
+    private void miami;
+    private void newYork;
+    private void chicago;
+    private void houston;
+    private void losAngeles;
+
+    public void getMiami() {
+        return miami;
+    }
+
+    public void setMiami(void miami) {
+        this.miami = miami;
+    }
+
+    public void getNewYork() {
+        return newYork;
+    }
+
+    public void setNewYork(void newYork) {
+        this.newYork = newYork;
+    }
+
+    public void getChicago() {
+        return chicago;
+    }
+
+    public void setChicago(void chicago) {
+        this.chicago = chicago;
+    }
+
+    public void getHouston() {
+        return houston;
+    }
+
+    public void setHouston(void houston) {
+        this.houston = houston;
+    }
+
+    public void getLosAngeles() {
+        return losAngeles;
+    }
+
+    public void setLosAngeles(void losAngeles) {
+        this.losAngeles = losAngeles;
+    }
+}
